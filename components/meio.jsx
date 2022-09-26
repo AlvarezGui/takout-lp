5bf4d49d472154a5c595b9a65d79c7d24ddf89d9
João Victor Rosinhole <61693806+joav12@users.noreply.github.com>
2022-09-26 16:48:13 -0300

    Add files via upload

--- components/meio.jsx
+++ components/meio.jsx
@@ -1,9 +1,12 @@
 export default function Meio(){
     return(
         <>
-            <div className="grid text-center">
-                <h1 className="">Feito simples para pequenos negócios</h1>
+            <div className="grid text-center diviTitulo1">
+                <h1 className="">Feito simples para</h1>
+                <h1 className="text-primary">pequenos negócios.</h1>
             </div>
+
+            <p className="grid text-center paragrafo1">Lorem ipsum dolor sit amet consectetur adipisicing elit.<br></br> Excepturi cupiditate iusto voluptatem distinctio animi hic ipsa culpa quos sit.</p>        
         </>
     );
 }
