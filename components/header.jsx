f058cae85bdd77ed66e7c1a8e64ce824a25dace4
João Victor Rosinhole <61693806+joav12@users.noreply.github.com>
2022-09-27 12:27:52 -0300

    Add files via upload

--- components/header.jsx
+++ components/header.jsx
@@ -3,14 +3,18 @@ import Image from 'next/image';
 export default function Header(){
     return(
         <>
+            {/* Navbar em si */}
             <div className="borderBaixo">
                 <nav className="navbar navbar-expand-lg  cora nav"> {/* trocar cor bg */}
+
+                    {/* Logo do header */}
                     <div className="container-fluid">
                         <a className="navbar-brand cor-letra imgTakout" href=""> <Image src="/Takout.png" width={90} height={20} alt='takout'></Image> </a>
                         <button  className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"></span>    
                         </button>    
 
+                        {/* Botões de navegação */}
                         <div className="collapse navbar-collapse" id="navbarNav">
                             <ul className="nav navLoca">
                                 <li className="nav-item nItem">
@@ -26,6 +30,7 @@ export default function Header(){
                         </div>
                     </div>  
 
+                    {/* Botão login */}
                     <div className="container-fluid justify-content-end btnDiv">
                         <button className=" btnL" type="button">Login</button>
                         <button className="btn btnI btn-primary me-2 cor-letra" type="button">  Iniciar  </button>
--- components/meio.jsx
+++ components/meio.jsx
@@ -1,12 +1,33 @@
 export default function Meio(){
     return(
         <>
+        {/* Titulo da págia */}
             <div className="grid text-center diviTitulo1">
                 <h1 className="">Feito simples para</h1>
                 <h1 className="text-primary">pequenos negócios.</h1>
             </div>
 
-            <p className="grid text-center paragrafo1">Lorem ipsum dolor sit amet consectetur adipisicing elit.<br></br> Excepturi cupiditate iusto voluptatem distinctio animi hic ipsa culpa quos sit.</p>        
+        {/* Subtitulo da págia */}
+            <div className="grid text-center paragrafo1">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.<br></br> Excepturi cupiditate iusto voluptatem distinctio animi hic ipsa culpa quos sit.</p>
+            </div>
+
+        {/* Ó buttão soh */}
+            <div className="text-center">
+                <button className="btn btnCont btn-primary me-2 cor-letra" type="button">Comece gratuitamente</button>    
+            </div>    
+
+        {/* sente só o PLACE HOLDER DO PAAAAAAAI */}
+            <div className="h-100 d-flex align-items-center justify-content-center">
+                <div className="placeHolder1"></div>
+            </div>     
+
+        {/* solução */}
+            <div className="soluBGC">
+                <div>
+                    <h2>Nossa solução para o seu negócio</h2>
+                </div> 
+            </div>          
         </>
     );
 }
